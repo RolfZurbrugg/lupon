0ec784bee45f74633d400e8fcb788923caa8b775
monkeyinabox <florian.rindlisbacher@students.bfh.ch>
2017-09-20 16:45:24 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home.jinja')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
