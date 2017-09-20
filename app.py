0e0fd46ed6549d772f59d817e9d0307b9a37896e
monkeyinabox <florian.rindlisbacher@students.bfh.ch>
2017-09-20 17:03:42 +0200

    update

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('home.jinja')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.jinja')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
