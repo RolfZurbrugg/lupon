958de24cccd4460c21b5f5efb0c9b092c60dc6d8
RolfZurbrugg <rolf.zurbrugg@gmail.com>
2017-09-26 17:55:33 +0200

    stuff

---
+++ lupon2.0/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dicta dolor eos esse ipsa magni maiores, minima minus nam, natus nihil numquam omnis, quae quaerat sequi soluta tempora ullam veritatis.',
+            'author':'Rolf Zur',
+            'created_date':'26.09.2017'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dicta dolor eos esse ipsa magni maiores, minima minus nam, natus nihil numquam omnis, quae quaerat sequi soluta tempora ullam veritatis.',
+            'author':'Bill Billson',
+            'created_date':'26.09.2017'
+        },
+        {
+            'id': 3,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dicta dolor eos esse ipsa magni maiores, minima minus nam, natus nihil numquam omnis, quae quaerat sequi soluta tempora ullam veritatis.',
+            'author':'Bob Bober',
+            'created_date':'26.09.2017'
+        }
+    ]
+    return articles
